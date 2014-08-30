60f543899e7366f44b15bd9ae5edd5814877f68b
Emmanouil Konstantinidis <manos@iamemmanouil.com>
2014-08-30 16:54:02 +0100

    Example Directive, Returns title

--- js/directives/example.js
+++ js/directives/example.js
@@ -1,2 +1,9 @@
 var directives = angular.module('directives.exampleDirective', []);
 
+directives.directive('pageTitle', function() {
+
+    return {
+      template: 'Page Title: {{ title }}'
+    };
+
+});
