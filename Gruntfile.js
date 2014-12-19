1ea4a96a0f4a4b978f51ab363e99621b878a0a75
Emmanouil Konstantinidis <manos@iamemmanouil.com>
2014-12-19 23:51:37 +0000

    Grunt Connect

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,25 +14,41 @@ module.exports = function(grunt) {
     },
 
     copy: {
-        main: {
-            files: [
-                {expand: true, cwd: "src/", src: 'index.html', dest: 'build/'},
-                {expand: true, cwd: "src/images/", src: '**', dest: 'build/images/'},
-                {expand: true, cwd: "lib/bootstrap/fonts/", src: '**', dest: 'build/fonts/'},
-                {expand: true, cwd: "src/fonts/", src: '**', dest: 'build/fonts/'},
-            ]
-        }
+      main: {
+        files: [
+          {expand: true, cwd: "src/templates/", src: '**', dest: 'build/templates/'},
+          {expand: true, cwd: "src/", src: 'index.html', dest: 'build/'},
+          {expand: true, cwd: "src/js/", src: '**', dest: 'build/js/'},
+          {expand: true, cwd: "src/images/", src: '**', dest: 'build/images/'},
+          {expand: true, cwd: "src/lib/font-awesome/css/", src: '**', dest: 'build/css/'},
+          {expand: true, cwd: "src/lib/font-awesome/fonts/", src: '**', dest: 'build/fonts/'},
+          {expand: true, cwd: "src/lib/bootstrap/fonts/", src: '**', dest: 'build/fonts/'},
+          {expand: true, cwd: "src/lib/angular/", src: '**', dest: 'build/lib/'},
+          {expand: true, cwd: "src/lib/ui-bootstrap/", src: '**', dest: 'build/lib/'},
+        ]
+      }
     },
 
     less: {
-        main: {
-          expand: true,
-          flatten: true,
-          src:  ['src/less/*.less'],
-          dest: 'build/css/',
-          ext: '.css'
-        }
+      main: {
+        expand: true,
+        flatten: true,
+        src:  ['src/less/*.less'],
+        dest: 'build/css/',
+        ext: '.css'
+      }
     },
+
+    connect: {
+      server: {
+        options: {
+          open: true,
+          base: 'build',
+          keepalive: true,
+          useAvailablePort: true,
+        }
+      }
+    }
 
   });
 
@@ -40,6 +56,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-connect');
 
   grunt.registerTask('default', ['watch']);
 
